a3d21e62cbdf52370630bd768a33dadd28135747
Naved Uddin <naveduddin83@gmail.com>
2024-07-26 18:06:46 +0530

    add carousel images

--- src/components/Pages/Home/HeroSection.jsx
+++ src/components/Pages/Home/HeroSection.jsx
@@ -6,12 +6,13 @@ const { Title, Paragraph } = Typography;
 const HeroSection = () => {
     return (
         <div className="relative h-max mb-4">
-            <Carousel autoplay className="h-full">
-                <div className="relative h-full">
+            <Carousel autoplay className="h-full max-h-[70vh]">
+                <div className="relative h-full max-h-[70vh]">
                     <img
-                        src="https://placehold.co/1200x600/crimson/white?text=Image+1"
+                        // src="https://placehold.co/1200x600/crimson/white?text=Image+1"
+                        src="https://img.freepik.com/free-photo/close-up-delicious-burger-meal_23-2151434076.jpg?t=st=1721996510~exp=1722000110~hmac=833fa91faaf7186026f2b9caa654c2cade326d04493f20c5729fde5e88856c05&w=740"
                         alt="Hero 1"
-                        className="object-cover w-full h-full"
+                        className="object-cover w-full h-full max-h-[70vh]"
                     />
                     <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
                         <div className="text-center text-white">
@@ -23,9 +24,9 @@ const HeroSection = () => {
                 </div>
                 <div className="relative h-full">
                     <img
-                        src="https://placehold.co/1200x600/007bff/ffffff?text=Image+2"
+                        src="https://img.freepik.com/free-photo/top-view-fast-food-mix-mozzarella-sticks-club-sandwich-hamburger-mushroom-pizza-caesar-shrimp-salad-french-fries-ketchup-mayo-cheese-sauces-table_141793-3998.jpg?t=st=1721997078~exp=1722000678~hmac=c72887f2067d1a1b9dd6dd6c0c445a71e44ac705026d306f389f3a690a5e8524&w=740"
                         alt="Hero 2"
-                        className="object-cover w-full h-full"
+                        className="object-cover w-full h-full max-h-[70vh]"
                     />
                     <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
                         <div className="text-center text-white">
