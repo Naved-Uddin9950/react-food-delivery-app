77bde464435bf922f7a1ee1123401f4138f34b2c
Naved Uddin <naveduddin83@gmail.com>
2024-07-29 14:06:56 +0530

    translate login page in spanish

--- src/components/Pages/Auth/Login.jsx
+++ src/components/Pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useTransition } from 'react';
 import { Navigate, NavLink } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,6 +7,7 @@ import { signInWithPopup, signOut, signInWithEmailAndPassword } from 'firebase/a
 import { notify } from '../../Utils/Notify';
 import google from '/images/google.svg';
 import { useAuth } from '../../../hooks/useAuth';
+import { useTranslation } from 'react-i18next';
 
 const Login = () => {
     const { user } = useAuth();
@@ -31,9 +32,9 @@ const Login = () => {
     const handleSignIn = async () => {
         try {
             const result = await signInWithPopup(auth, provider);
-            notify('success', 'Signed in successfully');
+            notify('success', t('notify.login_success'));
         } catch (error) {
-            notify('error', 'Error signing in');
+            notify('error', t('notify.login_error'));
             console.error('Error signing in:', error);
         }
     };
@@ -41,9 +42,9 @@ const Login = () => {
     const handleSignOut = async () => {
         try {
             await signOut(auth);
-            notify('success', 'Signed out successfully');
+            notify('success', t('notify.logout_success'));
         } catch (error) {
-            notify('error', 'Error signing out');
+            notify('error', t('notify.logout_error'));
             console.error('Error signing out:', error);
         }
     };
@@ -53,7 +54,7 @@ const Login = () => {
 
         try {
             await signInWithEmailAndPassword(auth, email, password);
-            notify('success', 'Login successfully');
+            notify('success', t('notify.login_success'));
             setEmail('');
             setPassword('');
         } catch (error) {
@@ -62,17 +63,19 @@ const Login = () => {
         }
     };
 
+    const { t } = useTranslation();
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-dark dark:bg-gray-700">
                 <h2 className="text-2xl font-bold text-center text-gray-900 dark:text-white">
-                    {user ? 'You are already signed in, want to Log Out?' : 'Log In'}
+                    {user ? t('Login.haveAccount') : t('Login.login')}
                 </h2>
                 {!user && (
                     <form className="space-y-6" onSubmit={formHandler}>
                         <div>
                             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300" htmlFor="email">
-                                Email
+                                {t('Signup.email')}
                             </label>
                             <input
                                 type="email"
@@ -85,7 +88,7 @@ const Login = () => {
                         </div>
                         <div>
                             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300" htmlFor="password">
-                                Password
+                                {t('Signup.password')}
                             </label>
                             <input
                                 type="password"
@@ -101,7 +104,7 @@ const Login = () => {
                                 type="submit"
                                 className="w-full px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600"
                             >
-                                Log In
+                                {t('Login.login')}
                             </button>
                         </div>
                     </form>
@@ -109,19 +112,19 @@ const Login = () => {
                 <div className='flex flex-row justify-center items-center gap-2'>
                     {user ? (
                         <button onClick={handleSignOut} className='w-full px-4 py-2 font-semibold text-white bg-red-500 rounded hover:bg-red-600'>
-                            Sign Out
+                            {t('Login.logout')}
                         </button>
                     ) : (
                         <button onClick={handleSignIn} className='w-full px-4 py-2 font-semibold text-white bg-blue-700 rounded hover:bg-blue-800 flex flex-row justify-center items-center gap-2 mt-[-1rem]'>
-                            Sign In with Google <img src={google} alt="google" className='h-6' />
+                            {t('Login.google')} <img src={google} alt="google" className='h-6' />
                         </button>
                     )}
                 </div>
                 {!user && (
                     <div className="text-sm text-center text-gray-600 dark:text-gray-300">
-                        Don't have an account?{' '}
+                        {t('Login.noAccount')}{' '}
                         <NavLink to="/signup" className="text-blue-500 hover:text-blue-600">
-                            Sign Up
+                            {t('Signup.signup')}
                         </NavLink>
                     </div>
                 )}
