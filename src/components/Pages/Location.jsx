e90b7b9faef5e5927668d79838cd287cfa88cf04
Naved Uddin <naveduddin83@gmail.com>
2024-07-29 11:25:34 +0530

    translate location page

--- src/components/Pages/Location.jsx
+++ src/components/Pages/Location.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import Loader from '../Utils/Loader';
+import { useTranslation } from 'react-i18next';
 
 function Location() {
   const [iframeLoaded, setIframeLoaded] = useState(false);
+  const { t } = useTranslation();
 
   return (
     <div className="py-8 px-2 sm:px-6 md:px-20 lg:px-36">
-      <h2 className="text-3xl font-bold text-center mb-8 dark:text-white">Location</h2>
+      <h2 className="text-3xl font-bold text-center mb-8 dark:text-white">{t('Location.location')}</h2>
       <div className="mb-8">
-        <h3 className="text-xl font-semibold mb-4 dark:text-white">Our Location</h3>
+        <h3 className="text-xl font-semibold mb-4 dark:text-white">{t('Location.heading')}</h3>
         <div className="relative rounded-lg overflow-hidden shadow-dark">
           {!iframeLoaded && (
             <div className="absolute inset-0 flex justify-center items-center bg-gray-200 dark:bg-gray-700">
-              <span className="text-gray-500 dark:text-gray-300">{ <Loader /> }</span>
+              <span className="text-gray-500 dark:text-gray-300">{<Loader />}</span>
             </div>
           )}
           <iframe
@@ -28,13 +30,13 @@ function Location() {
 
       <div className="text-lg leading-relaxed dark:text-gray-300">
         <p className="mb-4">
-          Address: 123 Main Street, Cityville, State, Country, Zip Code
+          {t('Location.address')}: {t('Footer.address')}
         </p>
         <p className="mb-4">
-          Contact: Phone: +1 123-456-7890 | Email: info@example.com
+          {t('Location.contact')}: {t('Location.phone')}: {t('Footer.phone')} | {t('Location.email')}: {t('Footer.email')}
         </p>
         <p className="mb-4">
-          Hours of Operation: Monday to Friday: 9:00 AM - 6:00 PM
+          {t('Location.operation')}: {t('Location.hours')}
         </p>
       </div>
     </div>
